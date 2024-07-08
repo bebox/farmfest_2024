f3946d5259cc0fdcc63fa15245005c71e60f7253
schef <zvanstefan@gmail.com>
2024-07-08 11:16:28 +0200

    added bass clef staff changer

--- lilypond_transformer.py
+++ lilypond_transformer.py
@@ -60,7 +60,7 @@ def bass_tones_only(path: str):
             convert_harmony_to_bass_tones_only(file_path)
             append_to_header(file_path, "titlex", "(Sparki)")
 
-def staff_modifier(path: str, modifier: str):
+def score_modifier(path: str, modifier: str):
     lines = read_lines_from_file(path)
     in_score = False
     for line_index in range(len(lines)):
@@ -98,38 +98,57 @@ def append_to_header(path, variable, text):
         lines[line_index] = line
     write_lines_to_file(lines, path)
 
+def staff_modifier(path: str, from_str: str, to_str: str):
+    lines = read_lines_from_file(path)
+    in_staff = False
+    for line_index in range(len(lines)):
+        line = lines[line_index]
+        if in_staff:
+            if line == '}\n':
+                in_staff = False
+            else:
+                if from_str in line:
+                    line = line.replace(from_str, to_str)
+        else:
+            if '\\staffOne =' in line:
+                in_staff = True
+        lines[line_index] = line
+    write_lines_to_file(lines, path)
+
 @app.command()
 def transpose_b(path: str):
     print("Transpose to Bb")
     if is_path_file(path):
-        staff_modifier(path, r"\transpose c d")
+        score_modifier(path, r"\transpose c d")
         append_to_header(path, "titlex", "(Bb)")
     else:
         for file_path in glob.glob(f"{path}/*.ly"):
-            staff_modifier(file_path, r"\transpose c d")
+            score_modifier(file_path, r"\transpose c d")
             append_to_header(file_path, "titlex", "(Bb)")
 
 @app.command()
 def transpose_eb(path: str):
     print("Transpose to Eb")
     if is_path_file(path):
-        staff_modifier(path, r"\transpose es c'")
+        score_modifier(path, r"\transpose es c'")
         append_to_header(path, "titlex", "(Eb)")
     else:
         for file_path in glob.glob(f"{path}/*.ly"):
-            staff_modifier(file_path, r"\transpose es c'")
+            score_modifier(file_path, r"\transpose es c'")
             append_to_header(file_path, "titlex", "(Eb)")
 
 @app.command()
 def transpose_bass(path: str):
     print("Transpose to BASS")
     if is_path_file(path):
-        staff_modifier(path, r"\clef bass \transpose c' c")
+        score_modifier(path, r"\clef bass \transpose c' c")
         append_to_header(path, "titlex", "(BASS)")
+        staff_modifier(path, r"\clef treble", r"\clef bass")
     else:
         for file_path in glob.glob(f"{path}/*.ly"):
-            staff_modifier(file_path, r"\clef bass \transpose c' c" )
+            score_modifier(file_path, r"\clef bass \transpose c' c" )
             append_to_header(file_path, "titlex", "(BASS)")
+            staff_modifier(file_path, r"\clef treble", r"\clef bass")
 
 
 @app.command()
